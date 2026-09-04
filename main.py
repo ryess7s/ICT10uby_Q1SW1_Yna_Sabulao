68c05c1621c74fc9d7c298333aaf63c830926015
Yna Francesca <sabulao.yr@obmontessori.edu.ph>
2026-09-04 11:32:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#SABULAO, NOEMAYR
+studname = "Luna Isabel Noemayr"
+studage = 15 #integer
+studheight1 = 168 #integer
+countries_visit = ["country, countryy, countryyy"]
+student_type = False #wwww
+stud_pref{
+    "color": "",
+    "car brand": "",
+    "shoe size": 9,
+    "bff": ""
+}
+stud_fruits = {
+    "fruit1",
+    "ffruit2",
+    "fruit3",
+    "fruit4",
+    "fruit5",
+}
+days_week = {
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+}
+
+print("Name:", studname)
+print("Age:", studage)
